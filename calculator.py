68872be2cdbf8e834a0cf779b142269aec1711d1
DenzelMalik <145759113+DenzelMalik@users.noreply.github.com>
2025-04-30 20:43:01 +0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+def calc(a, b, opt):
+    c = 0
+    
+    if opt == '+':
+        c = a + b
+    elif opt == '-':
+        c = a - b
+    elif opt == '*':
+        c = a * b
+    elif opt == '/':
+        c = a / b
+    else:
+        return None
+    
+    return c
+
+
+sum = calc(10, 20, '+')
+
+print(sum)
